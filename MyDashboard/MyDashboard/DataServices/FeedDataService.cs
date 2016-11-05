b6ced8007c6911fb39543291aebf8cd5c82e3048
CHRISTIESMAC\Christie <christie.weakly@comcast.net>
2016-11-05 16:43:31 -0600

    Changing evreything

---
+++ MyDashboard/MyDashboard/DataServices/FeedDataService.cs
@@ -0,0 +1,17 @@
+﻿using Dashboard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dashboard.DataServices
+{
+    public class FeedDataService
+    {
+        public static List<Feed> GetFeedsForCurrentUser()
+        {
+            int userId = Convert.ToInt32(HttpContext.Current.Session["UserId"]);
+            return new List<Feed>();
+        }
+    }
+}
